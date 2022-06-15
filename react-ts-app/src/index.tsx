d3ce57dec269f8337327232df4b954230b747d74
Brayden Babbitt <brayden@babbitts.net>
2022-06-15 11:54:58 -0600

    Switched to HashRouter

--- react-ts-app/src/index.tsx
+++ react-ts-app/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 // Routes
 import Home from './pages/Home';
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}/>
@@ -24,7 +24,7 @@ root.render(
         <Route path="/apps" element={<Apps />}/>
         <Route path="/contact" element={<Contact />}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
