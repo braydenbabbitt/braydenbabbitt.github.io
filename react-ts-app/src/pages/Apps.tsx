60c089ec6acfd39a176248d298aa691264175c58
Brayden Babbitt <brayden@babbitts.net>
2022-07-06 17:19:47 -0600

    Added Random Quote Machine to Apps

--- react-ts-app/src/pages/Apps.tsx
+++ react-ts-app/src/pages/Apps.tsx
@@ -10,6 +10,7 @@ function Apps() {
       <div className={styles.card_list}>
         <Card external={true} imagePath='timer-image.png' title='Countdown Timer' description='A simple countdown timer written in Flutter' href='/apps/timer' />
         <Card external={true} imagePath='spotify-logo.png' title='Spotify Algorithms' description='An app for creating algorithms to manipulate Spotify playlists written in Python. This is a WIP and currently has no UI.' href='https://github.com/braydenbabbitt/spotify_algorithms' />
+        <Card external={true} imagePath='random-quote-machine.png' title='Random Quote Machine' description='A simple app for generating a random quote and being able to share it written in ReactJS.' href='https://codepen.io/ghlennedgis/full/vYdoQag' />
       </div>
     </div>
   );
